a08009fbe95889509dd8540012b40eb288c1f24d
Jared Jewell <jaje9434@colorado.edu>
2022-03-12 13:01:56 -0700

    add router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,50 +2,25 @@ import { useEffect, useState } from "react";
 import User from "./components/User/User";
 import "./App.css";
 import Playlists from "./components/Playlists/Playlists";
+import { Route, Routes } from "react-router-dom";
 // import { api } from "./utilities/api";
 
 function App() {
-  const [isAuthed, setIsAuthed] = useState(false);
-
-  const click = () => {
-    localStorage.setItem("isAuthed", "yes");
-    setIsAuthed(true);
-  };
-
-  useEffect(() => {
-    switch (localStorage.getItem("isAuthed")) {
-      case "yes":
-        setIsAuthed(true);
-        break;
-      default:
-        setIsAuthed(false);
-    }
-  }, []);
-
   const heading = <h1>playlist shuffling extravaganza</h1>;
 
-  if (!isAuthed) {
-    return (
-      <div className="App">
-        {heading}
-        <a
-          onClick={() => {
-            click();
-          }}
-          href="http://localhost:8000/spotify/auth"
-        >
-          authorize us with spotify
-        </a>
-      </div>
-    );
-  }
-
   return (
-    <div className="App">
-      {heading}
-      <User />
-      <Playlists />
-    </div>
+    <Routes>
+      <Route
+        path="/playlists"
+        element={
+          <div className="App">
+            {heading}
+            <User />
+            <Playlists />
+          </div>
+        }
+      ></Route>
+    </Routes>
   );
 }
 
