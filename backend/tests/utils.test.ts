6a27e798c9b96ece25a18565c0739f87b9d46b6f
Jared Jewell <jaje9434@colorado.edu>
2022-03-15 20:48:14 -0600

    format util tests
    Signed-off-by: Jared Jewell <jaje9434@colorado.edu>

--- backend/tests/utils.test.ts
+++ backend/tests/utils.test.ts
@@ -9,33 +9,27 @@ function dataBuilder(data: string[]): sortInput[] {
   });
 }
 
-
 describe("Utility tests", () => {
   it("Should alphabetize properly formed data", () => {
     let data = ["b", "a", "d", "ba", "ar", "zo", "a", "nl", "sl", "za", "e"];
     let alphData = alphabetize(dataBuilder(data));
     let sorted = true;
 
-    for (let i = alphData.length-1; i > 1; i--) {
+    for (let i = alphData.length - 1; i > 1; i--) {
       let after = alphData[i].toCompare;
-      let before = alphData[i-1].toCompare;
+      let before = alphData[i - 1].toCompare;
       if (after < before) {
         sorted = false;
       }
     }
 
-    expect(sorted).toBe(true)
-
+    expect(sorted).toBe(true);
   });
 
   it("Should slice properly, even with out of bounds index", () => {
-    let data = [1,2,3,4,5]
-    let sliced = data.slice(0,20)
+    let data = [1, 2, 3, 4, 5];
+    let sliced = data.slice(0, 20);
 
-    expect(sliced.length).toEqual(data.length)
-  })
-
-
-
+    expect(sliced.length).toEqual(data.length);
+  });
 });
-
